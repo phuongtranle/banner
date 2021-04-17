a69667a1c6dafe64e7f5be960ebcfe3b1916ee82
Tran Le Phuong <duonglam8808@gmail.com>
2021-04-17 22:59:13 +0700

    change

--- default.js
+++ default.js
@@ -3,20 +3,47 @@ jQuery(function($) {
 
 
   var owl = $('.owl-carousel')
+
+  var fixOwl = function (event) {
+    $('.owl-carousel .owl-item').css({ width: 920 + 'px' })
+    var len = $('.owl-carousel .owl-item').length
+    $('.owl-carousel .owl-stage').css({ width: (920 * len) + 'px' })
+    var Wid_res = ($(window).width()-920) / 2;
+    var current = $('.owl-carousel').find('.center').index();
+
+    owl.find('.owl-stage').css({ "transform": "translate3d(" + ((-920*current) + Wid_res) + "px, 0px ,0px)" })
+    console.log((-920*current) + Wid_res)
+  }
+
+  owl.on('initialized.owl.carousel', function(event) {
+    fixOwl()
+  })
+
   owl.owlCarousel({
     center: true,
-    items:2,
     loop:true,
-    margin:10,
     responsive:{
-        600:{
-            items:2.2
-        }
-    }
+      1220:{
+          items:1.72
+      }
+  }
   });
 
+  owl.on('resized.owl.carousel refreshed.owl.carousel', function(event) {
+    $('.owl-carousel .owl-item').css({ width: 920 + 'px' })
+    var len = $('.owl-carousel .owl-item').length
+    $('.owl-carousel .owl-stage').css({ width: (920 * len) + 'px' })
+    var Wid_res = ($(window).width()-920) / 2;
+    var current = $('.owl-carousel').find('.center').index();
+    var respon = (-920*current) + Wid_res;
+    owl.find('.owl-stage').css('transform', 'translate3d('+((-920*current) + Wid_res)+'px, 0px,0px)');
+    console.log(respon)
+  })
+
+
   $('.owl-dots .owl-dot').click(function () {
-    $('.owl-carousel .owl-stage').css({ transform: 'translate(' + coordinate + 'px' + ',0px)' })
+    var coordinate = 1000;
+    owl.find('.owl-stage').css('transform', 'translate3d('+coordinate+'px, 0px,0px)');
     // $('.owl-carousel .owl-stage').css({width: 300+'px'})
     console.log($(this).index()+1)
 
